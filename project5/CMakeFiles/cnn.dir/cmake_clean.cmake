8d336320e644ca8d7cd0f3df58cbe7031d5e7a40
MQ_Adventure <82221347+Q-LMQ@users.noreply.github.com>
2021-12-20 16:05:56 +0800

    Add files via upload
    Project 5-A Simple CNN Model

---
+++ project5/CMakeFiles/cnn.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cnn.dir/cnn.cpp.o"
+  "cnn"
+  "cnn.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cnn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
